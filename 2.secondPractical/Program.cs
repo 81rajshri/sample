fa91d305b784a83cd11e220f00fee69b30377ba4
Rajshri_Rautray <rajshrirautray81@gmail.com>
2026-04-23 10:55:16 +0530

    C#

---
+++ 2.secondPractical/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static int Add(int a, int b) => a + b;
+    static int Sub(int a, int b) => a - b;
+    static int Mul(int a, int b) => a * b;
+    static int Div(int a, int b) => a / b;
+
+    // Advanced operations
+    static double Power(double a, double b) => Math.Pow(a, b);
+    static int Modulus(int a, int b) => a % b;
+
+    static void Main()
+    {
+        int choice;
+
+        do
+        {
+            Console.WriteLine("\n1.Add 2.Sub 3.Mul 4.Div 5.Power 6.Modulus 7.Exit");
+            Console.Write("Enter choice: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice >= 1 && choice <= 6)
+            {
+                Console.Write("Enter first number: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1: Console.WriteLine("Result: " + Add((int)a, (int)b)); break;
+                    case 2: Console.WriteLine("Result: " + Sub((int)a, (int)b)); break;
+                    case 3: Console.WriteLine("Result: " + Mul((int)a, (int)b)); break;
+                    case 4: Console.WriteLine("Result: " + Div((int)a, (int)b)); break;
+                    case 5: Console.WriteLine("Result: " + Power(a, b)); break;
+                    case 6: Console.WriteLine("Result: " + Modulus((int)a, (int)b)); break;
+                }
+            }
+
+        } while (choice != 7);
+    }
+}
---
+++ 4.practical/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 10, 25, 5, 40, 15 };
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        Console.WriteLine("Maximum: " + max);
+        Console.WriteLine("Minimum: " + min);
+    }
+}
